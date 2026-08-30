0fbc9e40d99ba35ce98793766ca0794e996f817d
Santiago <santiagogomezmendez6@gmail.com>
2026-08-30 15:08:17 -0600

    agregamos la funcionalidad de sum

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let age = 23;
 let message = "hello world";
 let cash = 2000;
 
+function sum (...nums) {
+  return nums.reduce((counter,num)=>counter + num,0);
+}
+
+console.log(sum(1,2,3));
+
 function saludar (user,age) {
   return `im ${user} have ${age} years`
 }
